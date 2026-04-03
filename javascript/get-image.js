4f982c765deb6f51df9705c822b5bc2c74126dc7
JuanPablo <juanpablolezama0@gmail.com>
2026-04-03 15:04:26 -0500

    Pagina Versión 1.1

--- javascript/get-image.js
+++ javascript/get-image.js
@@ -1,30 +1,49 @@
 import { error } from "node:console";
 import { cloudinary } from "../javascript/index.js";
 import strict from "node:assert/strict";
+import fs from 'fs';
+import { url } from "node:inspector";
 /////////////////////////////////////
 // Gets details of an uploaded image
 /////////////////////////////////////
 
-const mapInfo = new Map()
+async function generateJSON() {
 
-cloudinary.api
-.resources_by_asset_folder('prueba', { 
-    metadata: true, // Trae campos personalizados del fotógrafo
-    context: true,  // Trae metadatos adicionales (como descripciones o captions)
-})
-.then(result => {
-result.resources.forEach(img => {
+    try{
+        const result = await cloudinary.api.resources_by_tag("img1", {
+            context: true,
+        });
 
-    if (!mapInfo.has(img.public_id)){
-    mapInfo.set(img.public_id, new Map());
+        // Solo lo que necesita el HTML
+
+        const galleyData = result.resources.map(img => ({
+            url: img.secure_url,
+            name: img.display_name,
+            alt: img.context?.custom?.alt || img.display_name
+        }));
+
+        // Guardar el array
+
+        fs.writeFileSync('./resourcesJSON/fotos.json', JSON.stringify(galleyData, null, 2));
+
+        console.log("Archivo JSON generado con exito");
+
+    }catch(error){
+        console.error("Error al generar el Json:", error);
     }
-    const mapImg = mapInfo.get(img.public_id);
+    
+}
 
-    mapImg.set('url', img.url);
-    mapImg.set('contexto', img.context || "Sin descripción");
+cloudinary.api.tags()
+    .then(result => {
+        // 'result.tags' es un array de strings: ["img1", "proyecto1", "paisa"]
+        const misTags = result.tags;
+        
+        console.log("Etiquetas encontradas en la cuenta:", misTags);
+        
+        // Aquí podrías guardarlas en un JSON si quieres que tu nav se cree solo
+    })
+    .catch(err => console.error("Error al obtener tags:", err));
 
-    console.log(`Guardado en Map: ${img.public_id}`);
-});
-console.log("Estructura completa:", mapInfo);
-})
-.catch(err => console.error(err));
+generateJSON();
+
---
+++ javascript/script.js
@@ -0,0 +1,31 @@
+const galleryContainers= document.querySelectorAll('.group');
+
+document.querySelectorAll('.row-click').forEach(fila => {
+    fila.addEventListener('click', () => {
+        window.location.href = fila.dataset.href;
+    });
+});
+
+async function loadGallery() {
+    try {
+        const response = await fetch('../resourcesJSON/fotos.json');
+        const fotos = await response.json();
+
+        // Generamos el HTML con estructura de bloque (Figure)
+        const htmlCompleto = fotos.map(foto => `
+            <figure class="item-galeria">
+                <a href="../html/${foto.name}.html">
+                    <img class="imgFront" src="${foto.url}" alt="${foto.alt}">
+                </a>
+                <figcaption class="pie-foto">${foto.alt}</figcaption>
+            </figure>
+        `).join('');
+
+        galleryContainers.forEach(container => {
+            container.innerHTML = htmlCompleto;
+        });
+    } catch (error) {
+        console.error("Error cargando la galería:", error);
+    }
+}
+loadGallery();
